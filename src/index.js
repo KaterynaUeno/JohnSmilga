4c3f6c9711d1de931906d59553f00c5a99a10e95
Kateryna Ueno <katerynaueno@gmail.com>
2021-10-22 12:10:25 +0100

    Amazon best selling books

--- src/index.js
+++ src/index.js
@@ -1,9 +1,35 @@
-import React from 'react';
-import ReactDom from 'react-dom';
+import React from "react";
+import ReactDom from "react-dom";
 
-function Greeting(){
-  return <h4>this is katia and this is my first component</h4>;
+// function Greeting(){
+//   return <h4>this is katia and this is my first component</h4>;
+// }
+
+// ReactDom.render(<Greeting />, document.getElementById('root'));
+
+function BookList() {
+  return (
+    <section>
+      <Book />
+    </section>
+  );
 }
 
+const Book = () => {
+  return (
+    <article>
+      <Image />
+      <Title />
+    </article>
+  );
+};
 
-ReactDom.render(<Greeting />, document.getElementById('root'));
+const Image = () => (
+  <img
+    src="https://images-eu.ssl-images-amazon.com/images/I/71yBZOvXBVL._AC_UL200_SR200,200_.jpg"
+    alt=""
+  />
+);
+
+const Title = () => <h1>The Man Who Died Twice</h1>;
+ReactDom.render(<BookList />, document.getElementById("root"));
